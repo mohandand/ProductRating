e52e317a9b76287ca28138e60a4ffa860d4943e1
mohandand <mohandand@gmail.com>
2022-10-04 16:28:32 -0700

    1 file modified

--- StarRating.js
+++ StarRating.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 export default function StarRating() {
   const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
   return (
     <div className="star-rating">
       {[...Array(5)].map((star, index) => {
@@ -10,8 +11,10 @@ export default function StarRating() {
           <button
             type="button"
             key={index}
-            className={index <= rating ? 'on' : 'off'}
+            className={index <= (hover || rating) ? 'on' : 'off'}
             onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
           >
             <span className="star">&#9733;</span>
           </button>
